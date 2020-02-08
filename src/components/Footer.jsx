38a4ccfedf57cb32c51d78e0ef1505ae80a8e1df
ZakMos <z.zza@hotmail.com>
2020-02-08 16:47:36 +0100

    add footer with style

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p className="footer">&copy; 2020 Zakaria Moslim</p>
+      </footer>
+    );
+  }
+}
+
+
+export default Footer;
