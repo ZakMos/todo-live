3b423e6531829f8244a1f937630458c72124ab81
ZakMos <z.zza@hotmail.com>
2020-02-08 20:02:13 +0100

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboardList } from '@fortawesome/free-solid-svg-icons';
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <p className="main-header">Hello from Header</p>
+        <p className="main-header">Welcome to my TODO List Application
+          <span className="font-todo"><FontAwesomeIcon icon={faClipboardList} /></span>
+        </p>
       </header>
     );
   }
