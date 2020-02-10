1bb84112eef56d6b44d7e56aa406202afe3fb52b
ZakMos <z.zza@hotmail.com>
2020-02-10 08:32:29 +0100

    fix bug in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,27 +11,27 @@ class Footer extends Component {
           <div className="justify-content-center  d-flex">
             <ul className="list-unstyled ">
               <li className="list-inline-item">
-                <a href="https://www.instagram.com/zaa_aak/" className="link-social-anim" target="_blank">
+                <a href="https://www.instagram.com/zaa_aak/" className="link-social-anim" target="_blank" rel="noopener noreferrer">
                   <FontAwesomeIcon icon={faInstagram} size="2x"/>
                 </a>
               </li>
               <li className="list-inline-item">
-                <a href="https://twitter.com/zakaria_m" className="link-social-anim" target="_blank">
+                <a href="https://twitter.com/zakaria_m" className="link-social-anim" target="_blank" rel="noopener noreferrer">
                   <FontAwesomeIcon icon={faTwitterSquare} size="2x"/>
                 </a>
               </li>
               <li className="list-inline-item">
-                <a href="https://www.facebook.com/Zak.Mus" className="link-social-anim" target="_blank">
+                <a href="https://www.facebook.com/Zak.Mus" className="link-social-anim" target="_blank" rel="noopener noreferrer">
                   <FontAwesomeIcon icon={faFacebookSquare} size="2x"/>
                 </a>
               </li>
               <li className="list-inline-item">
-                <a href="https://www.linkedin.com/in/zakmos" className="link-social-anim" target="_blank">
+                <a href="https://www.linkedin.com/in/zakmos" className="link-social-anim" target="_blank" rel="noopener noreferrer">
                   <FontAwesomeIcon icon={faLinkedin} size="2x"/>
                 </a>
               </li>
               <li className="list-inline-item">
-                <a href="https://ZakMos.github.io" className="link-social-anim" target="_blank">
+                <a href="https://ZakMos.github.io" className="link-social-anim" target="_blank" rel="noopener noreferrer">
                   <FontAwesomeIcon icon={faInternetExplorer} size="2x" />
                 </a>
               </li>
