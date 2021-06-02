7352c8489992ceff02f6ab97042b525d8ed25f94
zakmos <z.zza@hotmail.com>
2021-06-02 07:24:03 +0200

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,38 +1,33 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitterSquare, faFacebookSquare, faLinkedin, faInstagram, faInternetExplorer } from '@fortawesome/free-brands-svg-icons';
+import { faCodepen, faGithubSquare, faLinkedin, faInternetExplorer } from '@fortawesome/free-brands-svg-icons';
 
 
 class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        <p>&copy; 2020 Zakaria Moslim</p>
+        <p>&copy; 2021 Zakaria Moslim</p>
           <div className="justify-content-center  d-flex">
             <ul className="list-unstyled ">
               <li className="list-inline-item">
-                <a href="https://www.instagram.com/zaa_aak/" className="link-social-anim" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faInstagram} size="2x"/>
+                <a href="https://codepen.io/ZakMos" className="link-social-anim" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faCodepen} size="1x"/>
                 </a>
               </li>
               <li className="list-inline-item">
-                <a href="https://twitter.com/zakaria_m" className="link-social-anim" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faTwitterSquare} size="2x"/>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="https://www.facebook.com/Zak.Mus" className="link-social-anim" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faFacebookSquare} size="2x"/>
+                <a href="https://github.com/ZakMos" className="link-social-anim" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faGithubSquare} size="1x"/>
                 </a>
               </li>
               <li className="list-inline-item">
                 <a href="https://www.linkedin.com/in/zakmos" className="link-social-anim" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faLinkedin} size="2x"/>
+                  <FontAwesomeIcon icon={faLinkedin} size="1x"/>
                 </a>
               </li>
               <li className="list-inline-item">
                 <a href="https://ZakMos.github.io" className="link-social-anim" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faInternetExplorer} size="2x" />
+                  <FontAwesomeIcon icon={faInternetExplorer} size="1x" />
                 </a>
               </li>
             </ul>
